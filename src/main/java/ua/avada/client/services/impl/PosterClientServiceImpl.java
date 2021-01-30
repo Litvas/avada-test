601866ff3fbaca708aa5223fdee839a7698e3d0f
olitviakov <olitviakov@datas-tech.com>
2021-01-30 19:33:15 +0200

    add security

--- src/main/java/ua/avada/client/services/impl/PosterClientServiceImpl.java
+++ src/main/java/ua/avada/client/services/impl/PosterClientServiceImpl.java
@@ -1,12 +1,14 @@
 package ua.avada.client.services.impl;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 import ua.avada.client.services.PosterClientService;
 import ua.avada.domain.Poster;
 import ua.avada.repository.PosterRepository;
 
 import java.util.List;
 
+@Service
 @RequiredArgsConstructor
 public class PosterClientServiceImpl implements PosterClientService<Poster> {
 
